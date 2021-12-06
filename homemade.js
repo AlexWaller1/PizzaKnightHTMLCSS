898adcb15f4655f42f0f096f1ce8cbd12606e30a
Alex Waller <alex@artofalexwaller.com>
2021-12-05 23:01:44 -0500

    homeMadeStart

--- homemade.js
+++ homemade.js
@@ -1,2 +1,10 @@
 // JavaScript for Pizza Knights Homemade Page
 console.log("Hello Oa");
+
+const homeMadeName = document.getElementById("recipe-name");
+
+const homeMadeDetails = document.getElementById("details");
+
+const homeMadeImages = document.getElementById("pizza-image");
+
+const homeMadeButton = document.getElementById("homemade-button");
