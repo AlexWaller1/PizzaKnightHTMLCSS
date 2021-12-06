42bdc92c7f45ed47876111386bd057827fd809fb
Alex Waller <alex@artofalexwaller.com>
2021-12-05 23:22:36 -0500

    homeMadeEventListener

--- homemade.js
+++ homemade.js
@@ -5,6 +5,47 @@ const homeMadeName = document.getElementById("recipe-name");
 
 const homeMadeDetails = document.getElementById("details");
 
-const homeMadeImages = document.getElementById("pizza-image");
+const homeMadeImage = document.getElementById("pizza-image");
 
 const homeMadeButton = document.getElementById("homemade-button");
+
+const homeMadeList = document.getElementById("homemade-list");
+
+const msg1 = document.getElementById("msg-1-homemade");
+
+let homePizzas = [];
+
+function displayPizzas() {
+  let i = 0;
+  for (; i < homePizzas.length; i++) {
+    const nameLi = document.createElement("h2");
+    nameLi.appendChild(
+      document.createTextNode(`
+    ${homePizzas[i].name}`)
+    );
+    homeMadeList.appendChild(nameLi);
+  }
+}
+
+displayPizzas();
+
+homeMadeButton.addEventListener("click", onSubmit);
+
+function onSubmit(e) {
+  if (homeMadeName.ariaValueMax.trim() == "") {
+    msg1.innerHTML = "Please Fill Out All Fields";
+  } else {
+    msg1.innerHTML = "Can't Wait To Try The Recipe!";
+    let newPizza = {
+      recipe: `${homeMadeName.value}`,
+      details: `${homeMadeDetails.value}`,
+      image: `${homeMadeImage.value}`
+    };
+    homePizzas.push(newPizza);
+    console.log(homePizzas);
+
+    homeMadeName.value = "";
+    homeMadeDetails.value = "";
+    homeMadeImage.value = "";
+  }
+}
