9e1043f75f5b5993616591a1bb62fa9940401f65
Alex Waller <alex@artofalexwaller.com>
2021-12-05 21:35:01 -0500

    pizzaPlaces

--- shops.js
+++ shops.js
@@ -21,25 +21,43 @@ console.log("------------------------------------------------");
 function displayShops() {
   for (let i = 0; i < pizzaPlaces.length; i++) {
     const nameLi = document.createElement("h2");
-    nameLi.id = "shop-name-header";
+    nameLi.className = "shop-name-header";
     nameLi.appendChild(
       document.createTextNode(`
-    ${pizzaPlaces[i]}`)
+    ${pizzaPlaces[i].name}`)
     );
     shopsList.appendChild(nameLi);
+    const addressLi = document.createElement("h3");
+    addressLi.className = "shop-address-header";
+    addressLi.appendChild(
+      document.createTextNode(`
+    ${pizzaPlaces[i].address}`)
+    );
+    shopsList.append(addressLi);
+    //.....................................................
+    const imageLi = document.createElement("img");
+    imageLi.id = "shop-image";
+    imageLi.src = `${pizzaPlaces[i].image}`;
+    imageLi.height = 500;
+    imageLi.width = 650;
+    shopsList.appendChild(imageLi);
   }
 }
 
 submitShopButton.addEventListener("click", onSubmit);
 
 function onSubmit(e) {
-  if (shopNameInput.value.trim() == "") {
+  if (shopNameInput.value.trim() == "" || shopAddressInput.value.trim() == "") {
     msg1.innerHTML = "Please Enter All Fields";
   } else {
     e.preventDefault();
     msg1.innerHTML = "Thanks For The Recommendation!";
     shopsList.innerHTML = "";
-    let newElement = `${shopNameInput.value}`;
+    let newElement = {
+      name: `${shopNameInput.value}`,
+      address: `${shopAddressInput.value}`,
+      image: `${shopImageInput.value}`
+    };
     pizzaPlaces.push(newElement);
     displayShops();
     console.log(pizzaPlaces);
