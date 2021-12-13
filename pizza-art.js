1c366a576c92213e0f61e2b6b76339ff6fbbabf4
Alex Waller <alex@artofalexwaller.com>
2021-12-13 17:49:41 -0500

    pizzaArt

--- pizza-art.js
+++ pizza-art.js
@@ -1,2 +1,18 @@
 // JavaScript for Pizza Knights Pizza Art Page
 console.log("Hello Oa");
+
+const artName = document.getElementById("art-name");
+
+const artImage = document.getElementById("art-image");
+
+const artDescription = document.getElementById("art-description");
+
+const artSubmitButton = document.getElementById("art-submit-btn");
+
+const pizzaArtList = document.getElementById("pizza-art-list");
+
+const pizzaArtMsg = document.getElementById("msg-1-art");
+
+let pizzaArt = [];
+
+let count = 0;
