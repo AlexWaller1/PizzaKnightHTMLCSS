8ef8cce05f00968eade6d280649ef42c4146d5c8
Alex Waller <alex@artofalexwaller.com>
2021-12-13 22:15:43 -0500

    pizzaUsers

--- users.js
+++ users.js
@@ -1,2 +1,22 @@
 // JavaScript for Pizza Knights Users Page
 console.log("Hello Oa");
+
+const userName = document.getElementById("user-name");
+
+const userLocation = document.getElementById("user-location");
+
+const userImage = document.getElementById("user-image");
+
+const userSubmitBtn = document.getElementById("user-submit-btn");
+
+const usersMsg = document.getElementById("mag-1-users");
+
+const usersList = document.getElementById("users-list");
+
+console.log("---------------------------------------------");
+
+let pizzaUsers = [];
+
+let count = 0;
+
+console.log("-----------------------------------------------");
