a40c9fc045d05e6dc9e67e3c5ee0b6da0af917e1
Alex Waller <alex@artofalexwaller.com>
2021-12-15 13:15:15 -0500

    pizzaStories

--- pizza-stories.js
+++ pizza-stories.js
@@ -32,6 +32,43 @@ if (data2 == null) {
   pizzaStories = data2;
   count = data4;
 }
+console.log("-------------------------------------------------------");
+
+function displayStories() {
+  pizzaStories.forEach(function (pizza1) {
+    // header for the story title
+    const newTitle = document.createElement("h2");
+    newTitle.className = "story-title";
+    newTitle.appendChild(document.createTextNode(`${pizza1.title}`));
+    storiesList.append(newTitle);
+    // header for story text
+    const newText = document.createElement("h3");
+    newText.className = "story-text";
+    newText.appendChild(document.createTextNode(`${pizza1.text}`));
+    storiesList.append(newText);
+    // image for story
+    const newImage = document.createElement("img");
+    newImage.className = "story-image";
+    newImage.src = `${pizza1.image}`;
+    newImage.width = 650;
+    newImage.height = 500;
+    storiesList.append(newImage);
+    // HTML breaks for good spaceing for button
+    const break1 = document.createElement("br");
+    storiesList.append(break1);
+    const break2 = document.createElement("br");
+    storiesList.append(break2);
+    // delete button for each entry
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "delete";
+    deleteButton.className = "story-delete-button";
+    storiesList.append(deleteButton);
+  });
+}
+
+displayStories();
+
+console.log("-----------------------------------------------------");
 
 storySubmitBtn.addEventListener("click", onSubmit);
 
@@ -51,6 +88,9 @@ function onSubmit(e) {
   let parseCount = JSON.stringify(count);
   localStorage.setItem("story-list", parseStory);
   localStorage.setItem("persist-count", parseCount);
+
+  storiesList.innerHTML = "";
+  displayStories();
 
   storyTitle.value = "";
   storyText.value = "";
